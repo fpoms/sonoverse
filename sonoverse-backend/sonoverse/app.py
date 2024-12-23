39c0f97fddb54026dbe9678a49ace15b3c77fd2a
Fait Poms <faitpoms@gmail.com@>
2024-12-23 15:54:37 -0800

    Simple helmholtz example solve and transfer to frontend

--- sonoverse-backend/sonoverse/app.py
+++ sonoverse-backend/sonoverse/app.py
@@ -1,7 +1,12 @@
+import json
+json.encoder.FLOAT_REPR = lambda x: format(x, '.10f')
+
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
 import bempp.api
+import numpy as np
+from bempp.api.linalg import gmres
 
 
 app = Flask(__name__)
@@ -10,9 +15,61 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/api/grid")
 def hello_world():
-    grid = bempp.api.shapes.cube(length=3, h=0.1)
+    # Form and solve helmholtz acoustic problem
+    k = 15.0
+    grid = bempp.api.shapes.sphere(r=1, h=0.5)
+    #grid = bempp.api.shapes.cube(length=1, h=0.5)
+    piecewise_const_space = bempp.api.function_space(grid, "DP", 0)
+    identity = bempp.api.operators.boundary.sparse.identity(
+        piecewise_const_space, piecewise_const_space, piecewise_const_space
+    )
+    adlp = bempp.api.operators.boundary.helmholtz.adjoint_double_layer(
+        piecewise_const_space, piecewise_const_space, piecewise_const_space, k
+    )
+    slp = bempp.api.operators.boundary.helmholtz.single_layer(
+        piecewise_const_space, piecewise_const_space, piecewise_const_space, k
+    )
+    lhs = 0.5 * identity + adlp - 1j * k * slp
+
+    @bempp.api.complex_callable
+    def combined_data(x, n, domain_index, result):
+        result[0] = 1j * k * np.exp(1j * k * x[0]) * (n[0] - 1)
+
+    grid_fun = bempp.api.GridFunction(piecewise_const_space, fun=combined_data)
+    neumann_fun, info = gmres(lhs, grid_fun, tol=1e-5)
+
+    # Produce domain of points to evaluate over
+    Nx = 400
+    Ny = 400
+    xmin, xmax, ymin, ymax = [-3, 3, -3, 3]
+    plot_grid = np.mgrid[xmin : xmax : Nx * 1j, ymin : ymax : Ny * 1j]
+    points = np.vstack((plot_grid[0].ravel(), plot_grid[1].ravel(), np.zeros(plot_grid[0].size)))
+    u_evaluated = np.zeros(points.shape[1], dtype=np.complex128)
+    u_evaluated[:] = np.nan
+
+    x, y, z = points
+    idx = np.sqrt(x**2 + y**2) > 1.0
+
+    # Evaluate domain points
+    from bempp.api.operators.potential import helmholtz as helmholtz_potential
+
+    slp_pot = helmholtz_potential.single_layer(piecewise_const_space, points[:, idx], k)
+    res = np.real(np.exp(1j * k * points[0, idx]) - slp_pot.evaluate(neumann_fun))
+    u_evaluated[idx] = res.flat
+    print('u_evaluated', u_evaluated.shape)
+    print('res', res.shape)
+
+    print(info)
     print(grid.vertices.shape)
     return {
         "vertices": grid.vertices.transpose(1, 0).flatten().tolist(),
-        "indices": grid.elements.transpose(1, 0).flatten().tolist()
+        "indices": grid.elements.transpose(1, 0).flatten().tolist(),
+        "evaluated": {
+            "x_bounds": [xmin, xmax],
+            "y_bounds": [ymin, ymax],
+            "width": Nx,
+            "height": Ny,
+            "z": 0,
+            "result": [f"{x:.6f}" for x in np.real(u_evaluated.T).tolist()]
+        }
     }
--- sonoverse-ui/src/App.tsx
+++ sonoverse-ui/src/App.tsx
@@ -2,8 +2,9 @@ import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import * as THREE from 'three'
-import { rotate } from 'three/tsl'
+import {min, max} from 'lodash';
 import axios from 'axios'
+import { OrbitControls } from '@react-three/drei'
 
 
 function Box(props: ThreeElements['mesh']) {
@@ -38,9 +39,19 @@ function Box(props: ThreeElements['mesh']) {
 
 function Grid() {
   const ref = useRef<THREE.Mesh>(null!)
+  const planeMeshRef = useRef<THREE.MeshBasicMaterial>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   const [geometry, setGeometry] = useState<THREE.BufferGeometry>(new THREE.SphereGeometry(3));
+  const [texture, setTexture] = useState<THREE.DataTexture>(new THREE.DataTexture(null, 200, 200));
+  const [planeSize, setPlaneSize] = useState<number[]>([0, 0]);
+
+
+  const RGB_Log_Blend=(p,c0,c1)=>{
+    const i=parseInt,r=Math.round,P=1-p,[a,b,c,d]=c0.split(","),[e,f,g,h]=c1.split(","),x=d||h,j=x?","+(!d?h:!h?d:r((parseFloat(d)*P+parseFloat(h)*p)*1000)/1000+")"):")";
+    return"rgb"+(x?"a(":"(")+r((P*i(a[3]=="a"?a.slice(5):a.slice(4))**2+p*i(e[3]=="a"?e.slice(5):e.slice(4))**2)**0.5)+","+r((P*i(b)**2+p*i(f)**2)**0.5)+","+r((P*i(c)**2+p*i(g)**2)**0.5)+j;
+  }
+
 
   useEffect(() => {
     axios.get('http://127.0.0.1:5000/api/grid')
@@ -52,6 +63,39 @@ function Grid() {
         geometry.setIndex( indices );
         geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
         setGeometry(geometry);
+
+        // Texture
+        const width = data.evaluated.width;
+        const height = data.evaluated.height;
+
+        setPlaneSize([data.evaluated.x_bounds[1] - data.evaluated.x_bounds[0],
+                      data.evaluated.y_bounds[1] - data.evaluated.y_bounds[0]]);
+
+        const rawData: number[] = data.evaluated.result.map(v => v == 'nan' ? 0 : +v);
+        const size = width * height;
+        const texData = new Uint8Array( 4 * size );
+        const arrayMin: number = min(rawData) ?? 0;
+        const arrayRange: number = (max(rawData) ?? 0) - arrayMin;
+
+        const color7 = "rgb(0,0,0, 255)";
+        const color8 = "rgba(200,30,20, 255)";
+
+        for ( let i = 0; i < size; i ++ ) {
+          const stride = i * 4;
+
+          const val = (rawData[i] - arrayMin)/arrayRange;
+          const color = new THREE.Color(RGB_Log_Blend(val, color7, color8));
+          
+          texData[ stride ] = color.r * 255;
+          texData[ stride + 1 ] = color.g * 255;
+          texData[ stride + 2 ] = color.b * 255;
+          texData[ stride + 3 ] = 255;
+        }
+
+        // used the buffer to create a DataTexture
+        const texture = new THREE.DataTexture( texData, width, height );
+        texture.needsUpdate = true;
+        setTexture(texture);
       })
       .catch(error => {
         console.error(error);
@@ -59,15 +103,20 @@ function Grid() {
   }, []);
 
   return (
-    <mesh
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-      geometry={geometry}>
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} wireframe={true} />
-    </mesh>
+    <>
+      <mesh
+        ref={ref}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+        geometry={geometry}>
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} wireframe={true} />
+      </mesh>
+      <mesh position={[0, 0, 0]} rotation={[0, 0, Math.PI/2]}>
+        <planeGeometry args={planeSize}/>
+        <meshBasicMaterial ref={planeMeshRef} attach="material" map={texture} />
+      </mesh>
+    </>
   )
 }
 
@@ -127,20 +176,22 @@ function App() {
         } else if (event.button == 1) {
             middleClick(true);
         }}}
-       onMouseUp={(event) => {
-        leftClick(false);
-        middleClick(false);
+        onMouseUp={(event) => {
+          leftClick(false);
+          middleClick(false);
         }} 
         onMouseOut={(event) => {
           leftClick(false);
           middleClick(false);
-          }} >
+         }} >
         <ambientLight intensity={Math.PI / 2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Grid/>
         <axesHelper args={[5]} />
-        <Pointer leftClicked={leftClicked} middleClicked={middleClicked}/>
+        {//<Pointer leftClicked={leftClicked} middleClicked={middleClicked}/>}
+}
+        <OrbitControls />
       </Canvas>
     </div>
   )
